1648d19e7c92bd7bf039b458ee1a662913e551e4
Aris Papadopoulos <aris.ppd@gmail.com>
2018-12-06 17:46:05 +0200

    Creates routes for main page and post page

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import rootReducer from './reducers';
 import thunk from 'redux-thunk';
 import './css/index.css';
 import App from './components/App';
+import SinglePost from './components/SinglePost';
 import * as serviceWorker from './serviceWorker';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(rootReducer)}>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/posts/:slug" component={SinglePost} />
+                <Route path="/" component={App} />
+            </Switch>
+        </BrowserRouter>
     </Provider>
 , document.getElementById('root'));
 
