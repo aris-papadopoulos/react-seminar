89df27e4538036ad8941590fffd2dc21a919aef4
Aris Papadopoulos <aris.ppd@gmail.com>
2018-12-06 18:00:41 +0200

    Removes header HTML from the 2 route components

--- src/components/App.js
+++ src/components/App.js
@@ -6,58 +6,22 @@ import '../css/App.css';
 import { getPosts } from '../actions/';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.clearNotifications = this.clearNotifications.bind(this);
-        this.state = {
-            newNotifications: 4
-        }
-    }
 
     componentDidMount() {
         this.props.getPosts();
     }
 
-    renderNotifications() {
-        if (this.state.newNotifications > 0) {
-            return <span className="new badge red">{this.state.newNotifications}</span>
-        }
-        else {
-            return
-        }
-    }
-
-    clearNotifications() {
-        this.setState({
-            newNotifications: 0
-        });
-    } 
-
     render() {
         return (
-        <div className="App">
-            <header className="App-header">
-            <nav>
-                <div className="nav-wrapper blue lighten-1">
-                    <div className="container">
-                        <a href="/" className="brand-logo">Ferries In Greece</a>
-                        <ul id="nav-mobile" className="right">
-                            <li><a href="#sass">sass</a></li>
-                            <li><a href="#less" onClick={this.clearNotifications}>less {this.renderNotifications()}</a></li>
-                            <li><a href="#css">css</a></li>
-                        </ul>
+            <div className="App">
+                <main>
+                    <div className="row container">
+                        {this.props.posts.map(post => {
+                            return <Post imageSrc={post.featured_media.url} title={post.title} excerpt={post.excerpt} />
+                        })}
                     </div>
-                </div>
-            </nav>
-            </header>
-            <main>
-                <div className="row container">
-                    {this.props.posts.map(post => {
-                        return <Post imageSrc={post.featured_media.url} title={post.title} excerpt={post.excerpt} />
-                    })}
-                </div>
-            </main>
-        </div>
+                </main>
+            </div>
         );
     }
 }
--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -24,7 +24,7 @@ class SinglePost extends Component {
             newNotifications: 0
         });
     }
-    
+
     createMarkup() {
         return {__html: this.props.excerpt};
     }
@@ -32,36 +32,22 @@ class SinglePost extends Component {
     render() {
         return (
             <div className="App">
-            <header className="App-header">
-            <nav>
-                <div className="nav-wrapper blue lighten-1">
-                    <div className="container">
-                        <a href="/" className="brand-logo">Ferries In Greece</a>
-                        <ul id="nav-mobile" className="right">
-                            <li><a href="#sass">sass</a></li>
-                            <li><a href="#less" onClick={this.clearNotifications}>less {this.renderNotifications()}</a></li>
-                            <li><a href="#css">css</a></li>
-                        </ul>
-                    </div>
+                <main>
+                    <div className="col s12 m6">
+                        <div className="card z-depth-2">
+                            <div className="card-image">
+                                <img src={this.props.imageSrc} alt="mpamphs" />
+                                <span className="card-title">{this.props.title}</span>
+                            </div>
+                            <div className="card-content" dangerouslySetInnerHTML={this.createMarkup()}>
+                            </div>
+                            <div className="card-action">
+                                <a href="#mpampis" className="blue-text text-darken-2">Read more</a>
+                            </div>
+                        </div>
                 </div>
-            </nav>
-            </header>
-            <main>
-                <div className="col s12 m6">
-                    <div className="card z-depth-2">
-                        <div className="card-image">
-                            <img src={this.props.imageSrc} alt="mpamphs" />
-                            <span className="card-title">{this.props.title}</span>
-                        </div>
-                        <div className="card-content" dangerouslySetInnerHTML={this.createMarkup()}>
-                        </div>
-                        <div className="card-action">
-                            <a href="#mpampis" className="blue-text text-darken-2">Read more</a>
-                        </div>
-                    </div>
-               </div>
-            </main>
-        </div>
+                </main>
+            </div>
         )
     }
 }
