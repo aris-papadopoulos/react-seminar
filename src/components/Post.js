8e81bf54a85bee73757de93f7a21c3f10822d040
Aris Papadopoulos <aris.ppd@gmail.com>
2018-12-06 16:23:34 +0200

    Post: Parse HTML for excerpt

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 
 class Post extends Component {
+    createMarkup() {
+        return {__html: this.props.excerpt};
+    }
     render() {
         return (
                 <div className="col s12 m6">
@@ -10,8 +13,8 @@ class Post extends Component {
                             <img src={this.props.imageSrc} alt="mpamphs" />
                             <span className="card-title">{this.props.title}</span>
                         </div>
-                        <div className="card-content">
-                            <p>{this.props.excerpt}</p>
+                        <div className="card-content" dangerouslySetInnerHTML={this.createMarkup()}>
+                            {/* {this.props.excerpt} */}
                         </div>
                         <div className="card-action">
                             <a href="#mpampis" className="blue-text text-darken-2">Read more</a>
