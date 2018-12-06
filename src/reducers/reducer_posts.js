9dacd03bf408c145611293e730bb94bbd2682fc2
Aris Papadopoulos <aris.ppd@gmail.com>
2018-12-06 12:15:45 +0200

    Posts reducer maps through the API response and keeps only the data needed

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,22 @@
 import { GET_POSTS } from '../actions';
+// import _ from 'lodash';
 
 export default function (state = [], action) {
     switch(action.type) {
         case GET_POSTS:
-            return action.payload;
+        let newState = action.payload.map(post => {
+            return {
+                id: post.id,
+                title: post.title.rendered,
+                excerpt: post.excerpt.rendered,
+                featured_media_id: post.featured_media,
+                slug: post.slug
+            }
+        });
+        
+        console.log(action.payload);
+        console.log(newState);
+            return newState;
         default:
             return state;
     }
