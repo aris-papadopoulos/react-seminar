1b7d27a3985ac90a775c8ba844aab92f3192b8ca
Aris Papadopoulos <aris.ppd@gmail.com>
2018-12-10 17:32:19 +0200

    Adds a chained request getting the featured media of the post too

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,11 +20,17 @@ export function getCurrentPost(slug) {
     const request = axios.get(`https://blog.ferriesingreece.com/wp-json/wp/v2/posts/?slug=${slug}`);
 
     return (dispatch) => {
+        // Initial Request using slug from url
         request.then(({data}) => {
+            // Second Request after getting post ID
+            const nextRequest = axios.get(`https://blog.ferriesingreece.com/wp-json/wp/v2/posts/${data[0].id}?_embed`);
+            nextRequest.then(({data}) => {
+            // Now dispatch the action
             dispatch({ 
                 type: GET_CURRENT_POST, 
                 payload: data
             })
         });
+    });
     };
 }
