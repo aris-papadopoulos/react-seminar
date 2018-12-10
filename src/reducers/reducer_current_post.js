14061de9aeee8b44f4c897ea31b2d28065f85b6b
Aris Papadopoulos <aris.ppd@gmail.com>
2018-12-10 17:42:40 +0200

    Changes reducer of current post to store only necessary data

--- src/reducers/reducer_current_post.js
+++ src/reducers/reducer_current_post.js
@@ -4,7 +4,12 @@ export default function (state = {}, action) {
     switch(action.type) {
         case GET_CURRENT_POST:
             console.log(action.payload);
-            return action.payload;
+            return {
+                title: action.payload.title.rendered,
+                content: action.payload.content.rendered,
+                imageSrc: action.payload._embedded['wp:featuredmedia'][0].media_details.sizes.medium_large.source_url,
+                imageAlt: action.payload._embedded['wp:featuredmedia'][0].title.rendered
+            };
         default:
             return state;
     }
